3907581fd463293ecf9106d0c325e4cbe5d943ff
KhyatiC25 <khyati.chandak@infoobjects.com>
2023-03-13 14:00:17 +0530

    First Commit

---
+++ src/main/java/com/infoobjects/UserDto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.infoobjects.UserDto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String usernameOrEmail;
+    private String password;
+}
---
+++ src/test/java/com/infoobjects/taskmanagment/TaskmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoobjects.taskmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
