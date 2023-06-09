024ee66af71e1da35ad2ba9c4eaaf856f27c143b
KhyatiC25 <khyati.chandak@infoobjects.com>
2023-06-09 16:38:08 +0530

    frist commit

--- src/main/java/com/infoobjects/Controller/AuthoController.java
+++ src/main/java/com/infoobjects/Controller/AuthoController.java
@@ -19,8 +19,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-
 @Controller // on presentation layer
 @RestController
 @RequestMapping("/api/auth") // bydefault method is get
